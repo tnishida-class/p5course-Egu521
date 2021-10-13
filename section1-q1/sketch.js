26e3741715765dd0b9d7c4b53b38e3ff2ab8cb33
Egu521 <ymgamba521@gmail.com>
2021-10-13 14:18:16 +0900

    change-section1-q1

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -1,18 +1,19 @@
 // 練習問題：神戸市のマーク
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(400, 400);
   background(255);
 
   // 空欄を埋めて神戸市のロゴマークを完成させよう
   noFill();
   strokeWeight(25);
   strokeCap(SQUARE);
+  stroke(0,128,0)
   arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  // BLANK[1]
+  arc(100 - 25, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
   const red = color(196, 0, 0);
-  const blue = color(14, 47, 146);
+  const blue = color(14, 47, 146)
   const green = color(22, 131, 46);
   const gray = color(77);
 }
