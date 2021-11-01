96ff466e60c2a8eb9d280ee0e1f91fcb6d60809d
Egu521 <ymgamba521@gmail.com>
2021-11-02 00:45:58 +0900

    change-section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,23 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
-
+const g=1
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(1000, 1000);
   count = 0;
   cycle = 100;
+
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  if (count < 50) {ellipse(width / 2, height / 2, count);
+
+  }// BLANK[1]
+else {
+  ellipse(width / 2, height / 2,  100 - count);
+}
+if(keyIsDown(LEFT_ARROW)){ width -= 10; }
+if(keyIsDown(RIGHT_ARROW)){width += 10; }
 }
