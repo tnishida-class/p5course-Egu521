0d6fd20e3ac0e8557e30574b6868c9633d041b75
Egu521 <ymgamba521@gmail.com>
2021-11-30 09:53:28 +0900

    update-finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -19,15 +19,14 @@ background(72, 188, 212);
 makeCloud(cloudx, cloudy);
 makeCloud(cloudx + 100, cloudy + 100);
 
-
 fill(a, b, c);
-stroke(0);
+stroke(255);
 triangle(100, y, 300, y, 200, y + 150);
 arc (200, y, 200, 200, 180, 360);
 triangle(200, y + 150, 190, y + 170, 210, y + 170);
 line(200, y + 170, 200, y + 250);
-y-=3;
-if (y < -250) {
+y-=5;
+if(y < -250) {
   y=600
   a = random(0, 255);
   b = random(0, 255);
@@ -37,7 +36,7 @@ if (y < -250) {
 if(keyIsDown(UP_ARROW)){ y -= 10; }
 if(keyIsDown(DOWN_ARROW)){ y += 6; }
 
-cloudx +=  0.2;
+cloudx += 0.2;
 
 
 }
